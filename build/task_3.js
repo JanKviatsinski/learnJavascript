489e8c1bb913ea3bcf513e5bab5b9ecff98c6a04
JanKviatsinski <yan5751839@gmail.com>
2020-12-22 09:23:10 +0300

    task2 task3 21.12.20

--- build/task_3.js
+++ build/task_3.js
@@ -13,45 +13,21 @@
         }
     }
 
-
     function getProperty (obj, i, ...restArg){
+        console.log(restArg.length)
         for ( let index in obj){
-            if (i === 0){
+            if (i === restArg.length){
                 console.log(obj[index])
-                console.log((restArg.length++))
             }
             if (obj[index] === obj[restArg[i]]){
-                getProperty(obj[index], i++)
+                getProperty(obj[index], i++,'grocery','pasta', 'white')
             }
         }
     }
 
-    console.log(getProperty(products,0,'grocery','groats', 'white'))
+getProperty(products,0,'grocery','pasta', 'white')
 
 
 
-    // function getProperty (obj, i, ...restArg){
-    //     for ( let index1 in obj){
-    //         if (obj[index1] === obj[restArg[i]])
-    //             for (let index2 in obj[index1]){
-    //                 if (obj[index1][index2] === obj[index1][restArg[1]]){
-    //                     for ( let index in obj[index1][index2])
-    //                         if (obj[index1][index2][index]===obj[index1][index2][restArg[2]]){
-    //                             console.log(obj[index1][index2][index])
-    //                         }
-    //                 }
-    //             }
-    //     }
-    // }
-    // for (let i = 0; i<=restArg.length; i++){
-    //     let newObj = {};
-    //console.log(obj[restArg[i]][restArg[1]][restArg[2]])
-    //     if (obj[restArg[i]]){
-    //         console.log(newObj[restArg[i]])
-    //         newObj.push(...obj[restArg[i]])
-    //         console.log(newObj)
-    //     } else {
-    //         console.log(newObj[restArg[i]])}
-    // }
 
 }())
