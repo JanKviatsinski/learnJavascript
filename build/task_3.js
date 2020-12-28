207ecfdf25ca5a2486e3a471e46d72f59c449743
JanKviatsinski <yan5751839@gmail.com>
2020-12-28 14:55:57 +0300

    task 3 + modal

--- build/task_3.js
+++ build/task_3.js
@@ -13,70 +13,27 @@
         }
     }
 
-    // function getProperty (obj, i, ...restArg){
-    //     console.log(restArg.length)
-    //     for ( let index in obj){
-    //         if (i === restArg.length){
-    //             console.log(obj[index])
-    //         }
-    //         if (obj[index] === obj[restArg[i]]){
-    //             getProperty(obj[index], i++,'grocery','pasta', 'white')
-    //         }
-    //     }
-    // }
-    // function getProperty (obj, i, ...restArg){
-    //     for ( let index1 in obj){
-    //         if (obj[index1] === obj[restArg[i]])
-    //             for (let index2 in obj[index1]){
-    //                 if (obj[index1][index2] === obj[index1][restArg[++i]]){
-    //                     for ( let index in obj[index1][index2])
-    //                         if (obj[index1][index2][index]===obj[index1][index2][restArg[i + 1]]){
-    //                             return obj[index1][index2][index]
-    //                         }
-    //                 }
-    //             }
-    //     }
-    // }
+    function getProperty (obj, str){
+        let newObj = obj;
 
-    function getProperty (obj, i, ...restArg){
-        for ( let index1 in obj){
-            if (obj[index1] === obj[restArg[i]])
-                for (let index2 in obj[index1]){
-                    if (obj[index1][index2] === obj[index1][restArg[++i]]){
-                        for ( let index in obj[index1][index2])
-                            if (obj[index1][index2][index]===obj[index1][index2][restArg[i + 1]]){
-                                return obj[index1][index2][index]
-                            }
-                    }
-                }
+        let direction = str.split('.');
+
+        for (let i = 0; i < direction.length; i++){
+            if (newObj[direction[i]] === undefined){
+                newObj = undefined;
+                break;
+            }
+             else if (newObj[direction[i]]){
+                newObj = newObj[direction[i]];
+            } else {
+                newObj = undefined;
+            }
         }
+
+        return newObj
     }
 
-console.log(getProperty(products,0,'grocery','pasta', 'amount'))
 
-
-    // function getProperty (obj, i, ...restArg){
-    //     for ( let index1 in obj){
-    //         if (obj[index1] === obj[restArg[i]])
-    //             for (let index2 in obj[index1]){
-    //                 if (obj[index1][index2] === obj[index1][restArg[1]]){
-    //                     for ( let index in obj[index1][index2])
-    //                         if (obj[index1][index2][index]===obj[index1][index2][restArg[2]]){
-    //                             console.log(obj[index1][index2][index])
-    //                         }
-    //                 }
-    //             }
-    //     }
-    // }
-    // for (let i = 0; i<=restArg.length; i++){
-    //     let newObj = {};
-    //console.log(obj[restArg[i]][restArg[1]][restArg[2]])
-    //     if (obj[restArg[i]]){
-    //         console.log(newObj[restArg[i]])
-    //         newObj.push(...obj[restArg[i]])
-    //         console.log(newObj)
-    //     } else {
-    //         console.log(newObj[restArg[i]])}
-    // }
+console.log(getProperty(products,'grocery.groats.white'))
 
 }())
--- build/task_6.js
+++ build/task_6.js
@@ -41,5 +41,5 @@ let newArray = data.reduce((previousValue, item) =>{
 },{})
 
 console.log(newArray)
-
+    
 }())
