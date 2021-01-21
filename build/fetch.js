dc35da4dd6be728f236ef3d44281349d474dbcb6
JanKviatsinski <yan5751839@gmail.com>
2021-01-21 14:40:38 +0300

    21.01.21

--- build/fetch.js
+++ build/fetch.js
@@ -1,126 +1,62 @@
 (function (){
-// async function ff(){
-//     let usersPosts;
-//
-//     try {
-//         let responseName = await fetch(
-//             'https://jsonplaceholder.typicode.com/users/');
-//         let users = await responseName.json();
-//
-//         usersPosts = users.reduce((obj,next) =>{
-//             obj[next.id] = [next.name];
-//             return obj;
-//         },[])
-//
-//         // console.log(usersPosts)
-//     } catch (e) {
-//         console.log("Ошибка HTTP: " + e)
-//     }
-//
-//     let responseTitle = await fetch(
-//         'https://jsonplaceholder.typicode.com/posts');
-//
-//     let posts = await responseTitle.json();
-//
-//     posts.reduce((prev, next) =>{
-//         if (usersPosts[next.id]){
-//             usersPosts[next.id].push(next.title);
-//             usersPosts[next.id].push(next.body);
-//         }
-//     })
-//
-//     return usersPosts;
-// }
-//
-// ff().then(data => {
-//     const table = document.createElement('table');
-//     const tHead = table.createTHead();
-//     const tHeadRow = tHead.insertRow()
-//     const tHeadName = tHeadRow.insertCell();
-//     const tHeadTitle = tHeadRow.insertCell();
-//     const tHeadPost = tHeadRow.insertCell();
-//     let tBody = table.createTBody();
-//
-//     tHeadName.textContent = "Name";
-//     tHeadTitle.textContent = "Title";
-//     tHeadPost.textContent = "Post";
-//
-//     tBody = data.reduce((prev, next) =>{
-//         const tBodyRow =  prev.insertRow();
-//         const tBodyName = tBodyRow.insertCell()
-//         const tBodyTitle = tBodyRow.insertCell()
-//         const tBodyPost = tBodyRow.insertCell()
-//         tBodyName.textContent = next[0];
-//         tBodyTitle.textContent = next[1];
-//         tBodyPost.textContent = next[2];
-//         return prev;
-//     },tBody)
-//
-//     document.body.append(table)
-// })
+async function compilingPostsData(){
+    let usersPosts;
 
-    async function ff(){
-        let usersPosts;
-
-        const urls = [
+    const urls = [
             'https://jsonplaceholder.typicode.com/users',
             'https://jsonplaceholder.typicode.com/posts',
-        ]
+    ];
 
-        const requests = urls.map(url => fetch(url));
+    const requests = urls.map(url => fetch(url));
 
-        Promise.all(requests).then(async data => {
-            let [responseNames, responsePosts] = data;
+    const responses = await Promise.all(requests);
 
-            const users = await responseNames.json();
-            console.log(users)
+    let [responseNames, responsePosts] = responses;
 
-            usersPosts = users.reduce((obj, next) => {
+    const allNames = await responseNames.json();
+
+    usersPosts = allNames.reduce((obj, next) => {
                 obj[next.id] = [next.name];
                 return obj;
-            }, []);
+                }, []);
 
-            let posts = await responsePosts.json();
+    const allPosts = await responsePosts.json();
 
-            posts.reduce((prev, next) => {
-                if (usersPosts[next.id]) {
-                    usersPosts[next.id].push(next.title);
-                    usersPosts[next.id].push(next.body);
-                }
-            })
-            console.log(usersPosts)
-        })/*.then(d => {
-            return usersPosts
-        })*/
-
-        // return usersPosts;
+    for(let user of allPosts){
+        if (usersPosts[user.id]) {
+            usersPosts[user.id].push(user.title);
+            usersPosts[user.id].push(user.body);
+        }
     }
 
-    ff().then(data => {console.log(data)
-        // const table = document.createElement('table');
-        // const tHead = table.createTHead();
-        // const tHeadRow = tHead.insertRow()
-        // const tHeadName = tHeadRow.insertCell();
-        // const tHeadTitle = tHeadRow.insertCell();
-        // const tHeadPost = tHeadRow.insertCell();
-        // let tBody = table.createTBody();
-        //
-        // tHeadName.textContent = "Name";
-        // tHeadTitle.textContent = "Title";
-        // tHeadPost.textContent = "Post";
-        //
-        // data.reduce((prev, next) =>{
-        //     const tBodyRow =  prev.insertRow();
-        //     const tBodyName = tBodyRow.insertCell()
-        //     const tBodyTitle = tBodyRow.insertCell()
-        //     const tBodyPost = tBodyRow.insertCell()
-        //     tBodyName.textContent = next[0];
-        //     tBodyTitle.textContent = next[1];
-        //     tBodyPost.textContent = next[2];
-        //     return prev;
-        // },tBody)
-        //
-        // document.body.append(table)
-    })
+    return usersPosts;
+}
+
+compilingPostsData().then(data => {
+    const table = document.createElement('table');
+    const tHead = table.createTHead();
+    const tHeadRow = tHead.insertRow()
+    const tHeadName = tHeadRow.insertCell();
+    const tHeadTitle = tHeadRow.insertCell();
+    const tHeadPost = tHeadRow.insertCell();
+    let tBody = table.createTBody();
+
+    tHeadName.textContent = "Name";
+    tHeadTitle.textContent = "Title";
+    tHeadPost.textContent = "Post";
+
+    data.reduce((prev, next) =>{
+        const tBodyRow =  prev.insertRow();
+        const tBodyName = tBodyRow.insertCell();
+        const tBodyTitle = tBodyRow.insertCell();
+        const tBodyPost = tBodyRow.insertCell();
+        tBodyName.textContent = next[0];
+        tBodyTitle.textContent = next[1];
+        tBodyPost.textContent = next[2];
+        return prev;
+        },tBody)
+
+    document.body.append(table);
+})
 
 }())
