08d60ede44dd3dab9b2e3462cc8921cb6747433c
JanKviatsinski <yan5751839@gmail.com>
2021-02-24 19:18:56 +0300

    3

--- build/Inheritance.js
+++ build/Inheritance.js
@@ -21,24 +21,51 @@ Builder.prototype.plus = function (...addedData) {
 class IntBuilder extends Builder {
     constructor(int) {
         super(int);
-        this.int = int;
     }
 
-    minus (...values) {
+    minus = (...values) => {
         const subtractedValue = [...values].reduce((acc, current) => {
             acc = acc + current;
             return acc;
         })
 
-        this.int
-        return this.int - subtractedValue;
+        this.data = this.data - subtractedValue;
+        return this.data;
+    }
+
+    multiply = (value) => {
+        this.data = this.data * value;
+        return this.data;
+    }
+
+    divide = (value) => {
+        this.data = Math.trunc(this.data / value);
+        return this.data;
+    }
+
+    mod =  (value) => {
+        this.data = value % this.data;
+        return this.data;
     }
 }
 
 const a = new IntBuilder(10);
-a.plus(1);
-a.minus(1);
-console.log(a.minus(1));
+a.plus(2);
+console.log(a.get());
+a.minus(2);
+console.log(a.get());
+a.multiply(2);
+console.log(a.get());
+a.divide(6);
+console.log(a.get());
+a.mod(5);
+console.log(a.get());
+
+
+
+
+
+
 
 
 
