9ef51616c27599c38559aa6a4ee1344c689987e5
JanKviatsinski <yan5751839@gmail.com>
2021-03-05 19:52:34 +0300

    8 поломал дерево

--- build/base2.js
+++ build/base2.js
@@ -18,43 +18,56 @@
             return this.root
         }
 
-        insert(key, value) {
-            let currentNode = this.root
-
-            if (!currentNode) {
-                this.root = new Node(key, value)
+        insert(insertKey, insertValue) {
+            if (!this.root) {
+                this.root = new Node(insertKey, insertValue)
                 return
             }
 
-            chooseDirection(currentNode)
+            let foundNode = null
 
-            function chooseDirection(node) {
-                if (node.key < key) {
-                    goRight(node)
-                } else {
-                    goLeft(node)
-                }
+            const parentNode = chooseNode(this.root)
+
+            if (insertKey < parentNode.key) {
+                parentNode.left = new Node(insertKey, insertValue)
+            } else {
+                parentNode.right = new Node(insertKey, insertValue)
             }
 
-            function goRight(currentNode) {
+            function chooseNode(node) {
+
+                if (node.key < insertKey) {
+                    searchRight(node)
+                } else {
+                    searchLeft(node)
+                }
+
+                return foundNode
+            }
+
+
+            function searchRight(currentNode) {
+
                 if (currentNode.right) {
-                    chooseDirection(currentNode.right)
+                    chooseNode(currentNode.right)
                 } else {
-                    currentNode.right = new Node(key, value)
+                    foundNode = currentNode/*.right = new Node(insertKey, insertValue)*/
                 }
             }
 
-            function goLeft(currentNode) {
+            function searchLeft(currentNode) {
                 if (currentNode.left) {
-                    chooseDirection(currentNode.left)
+                    chooseNode(currentNode.left)
                 } else {
-                    currentNode.left = new Node(key, value)
+                    foundNode = currentNode/*.left = new Node(insertKey, insertValue)*/
                 }
             }
-            return this
         }
 
-        delete(key){
+        delete(key) {
+        }
+
+        search(key) {
 
         }
     }
@@ -63,12 +76,49 @@
 
     // bst.insert(2, 'two').insert(1, 'one').insert(3, 'three');
 
-    bst.insert(2, 'one')
-    bst.insert(1, 'one')
+    bst.insert(4, 'one')
     bst.insert(3, 'one')
-    bst.insert(6, 'one')
-    bst.insert(7, 'one')
+    bst.insert(1, 'one')
     bst.insert(5, 'one')
+    bst.insert(8, 'one')
+    bst.insert(7, 'one')
+    bst.insert(10, 'one')
+
     console.log(bst.roo());
+
+
+    // insert(key, value) {
+    //     if (!this.root) {
+    //         this.root = new Node(key, value)
+    //         return
+    //     }
+    //
+    //     chooseDirection(this.root)
+    //
+    //     function chooseDirection(node) {
+    //         if (node.key < key) {
+    //             goRight(node)
+    //         } else {
+    //             goLeft(node)
+    //         }
+    //     }
+    //
+    //     function goRight(currentNode) {
+    //         if (currentNode.right) {
+    //             chooseDirection(currentNode.right)
+    //         } else {
+    //             currentNode.right = new Node(key, value)
+    //         }
+    //     }
+    //
+    //     function goLeft(currentNode) {
+    //         if (currentNode.left) {
+    //             chooseDirection(currentNode.left)
+    //         } else {
+    //             currentNode.left = new Node(key, value)
+    //         }
+    //     }
+    //     return this
+    // }
 
 }())
